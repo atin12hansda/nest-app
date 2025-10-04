0fa6c23957e7668c342144615613600fa0425a7b
atin2025 <atin.12.hansda@icloud.com>
2025-10-04 22:06:04 +0530

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+# Dockerfile for NestJS app
+FROM node:18-alpine
+WORKDIR /app
+COPY package*.json ./
+RUN npm install --production
+COPY . .
+RUN npm run build
+EXPOSE 3000
+CMD ["node", "dist/main.js"]
---
+++ dist/main.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@nestjs/core");
+const app_module_1 = require("./app.module");
+async function bootstrap() {
+    const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    const { SwaggerModule, DocumentBuilder } = await import('@nestjs/swagger');
+    const config = new DocumentBuilder()
+        .setTitle('NestJS API')
+        .setDescription('API documentation')
+        .setVersion('1.0')
+        .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+    await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
+//# sourceMappingURL=main.js.map
